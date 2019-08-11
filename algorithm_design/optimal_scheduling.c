1ce379cdcfc5875cb7daede7a3831aef641f85be
thomas <thomas.matecki@gmail.com>
2019-08-10 23:14:07 -0400

    save

--- algorithm_design/optimal_scheduling.c
+++ algorithm_design/optimal_scheduling.c
@@ -28,10 +28,21 @@ typedef struct IntervalSet {
   _s_interval_set_node *head;
 } interval_set;
 
+typedef struct NodeCounter {
+  _s_interval_set_node *node;
+  int counter;
+} node_counter;
+
+/*
+ *
+ */
 static void print_interval(s_interval *interval) {
   printf("%d ... %d\n", interval->from, interval->to);
 }
 
+/*
+ *
+ */
 int max_height(_s_interval_set_node *node_a, _s_interval_set_node *node_b) {
   int a_depth = CHECKED_HEIGHT(node_a);
   int b_depth = CHECKED_HEIGHT(node_b);
@@ -42,6 +53,10 @@ int max_height(_s_interval_set_node *node_a, _s_interval_set_node *node_b) {
     return b_depth;
   }
 }
+
+/*
+ *
+ */
 static interval_set *new_set(void) {
   interval_set *set = malloc(sizeof(interval_set));
   set->head = NULL;
@@ -100,17 +115,23 @@ static int get_balance(_s_interval_set_node *node) {
 }
 
 /*
- * Recursively traverses `node` and inserts `interval` at the
+ * Recursively traverse `node` and inserts `interval` at the
  * appropriate position.
  */
 static _s_interval_set_node *insert(_s_interval_set_node *node,
                                     s_interval *interval, int d) {
+
+  // Recursively descend and insert a leaf in the tree.
   if (!node) {
     node = malloc(sizeof(_s_interval_set_node));
     node->val = interval;
     node->left = NULL;
     node->right = NULL;
-  } else if (node->val->from <= interval->from) {
+    /* Sort the tree using interval `to` date; this is the
+     * 'completion' of the interval in the optimal scheduling
+     * algorithm.
+     */
+  } else if (node->val->to < interval->to) {
     node->left = insert(node->left, interval, d + 1);
   } else {
     node->right = insert(node->right, interval, d + 1);
@@ -119,14 +140,14 @@ static _s_interval_set_node *insert(_s_interval_set_node *node,
   // Increment the height
   node->height = 1 + max_height(node->right, node->left);
 
+  // Rebalance the tree
   int balance = get_balance(node);
-  // printf("node at %p is height %d and with balance %d\n", node, node->height,
-  // balance);
 
   if (balance > 1) {
     // The left branch is too heavy
     if (get_balance(node->left) < 0) {
-      /* Left/Right heavier than Left/Left,
+
+      /* Left/Right is heavier than Left/Left,
        * (1) Rotate A(first left) to the left.
        * (2) Rotate C(the root) to the right.
        *     C           C           D
@@ -139,8 +160,10 @@ static _s_interval_set_node *insert(_s_interval_set_node *node,
        */
       return rotate_left(node->left);
     }
-    // Just rotate right; (2) above.
+
+    // Left/Left is heavier than Left/Right; Just rotate right - (2) above.
     return rotate_right(node);
+
   } else if (balance < -1) {
     // the right branch is too heavy; Do the opposite of above
     if (get_balance(node->right) > 0) {
@@ -148,26 +171,53 @@ static _s_interval_set_node *insert(_s_interval_set_node *node,
     }
     return rotate_left(node);
   } else {
+    // The tree is balanced
     return node;
   }
 }
-static void print_set_node(_s_interval_set_node *node, int idx) {
-  int i;
 
-  if (node) {
-    for (i = 0; i < idx; i++) {
-      printf(" ");
-    }
+static node_counter descend_left(_s_interval_set_node *node, int to_height) {
+  node_counter left_most = {NULL, 0};
 
-    //print_interval(node->val);
-    printf(".\n");
-    print_set_node(node->right, idx + 1);
-    print_set_node(node->left, idx + 1);
+  if (node && node->left) {
+    left_most = descend_left(node->left, to_height);
+    left_most.counter++; // Count height from the min in the tree
   }
+  /* Otherwise, node is the left most node(or the set is empty). In both
+   * cases return {NULL, 0}
+   */
+
+  if (to_height == left_most.counter) {
+    // Pass the node at to_height up the backtrace.
+    left_most.node = node;
+  }
+
+  return left_most;
 }
 
+/*
+ *
+ */
 static void set_add(interval_set *set, s_interval *interval) {
   set->head = insert(set->head, interval, 0);
+}
+
+/*
+ *
+ */
+static s_interval *set_pop_min(interval_set *set) {
+  node_counter counted_node = descend_left(set->head, 1);
+  _s_interval_set_node *min_node = counted_node.node->left;
+
+  s_interval * ret = min_node->val;
+
+  if (min_node->right) {
+    counted_node.node->left = min_node->right;
+  } else {
+    counted_node.node->left = NULL;
+  }
+
+  return ret;
 }
 
 s_interval *parse_line(char *line) {
@@ -180,6 +230,24 @@ s_interval *parse_line(char *line) {
   i0->from = atoi(line);
   i0->to = atoi(split_ptr + 1);
   return i0;
+}
+
+/*
+ *
+ */
+static void print_set_node(_s_interval_set_node *node, int idx) {
+  int i;
+
+  if (node) {
+    for (i = 0; i < idx; i++) {
+      printf(" ");
+    }
+
+    // print_interval(node->val);
+    printf(".\n");
+    print_set_node(node->right, idx + 1);
+    print_set_node(node->left, idx + 1);
+  }
 }
 
 int main() {
