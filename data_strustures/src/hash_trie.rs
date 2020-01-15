ddb084ca2cd2691af88b2f7b2cb71434d46116ae
Thomas Matecki <thomas@marianatek.com>
2020-01-15 06:59:36 -0500

    Okay, this is rust, I guess

--- data_strustures/src/hash_trie.rs
+++ data_strustures/src/hash_trie.rs
@@ -15,110 +15,119 @@ fn calc_hash<T: Hash>(t: &T) -> u64 {
     s.finish()
 }
 
-fn sub_hash4(h: &u64, offset: u8) -> usize {
+fn sub_hash(hash: &u64, offset: u8) -> usize {
     const SUB_HASH_LENGTH: u8 = 4;
     const SUBHASH_PAD: u8 = 64 - SUB_HASH_LENGTH;
 
     let bit_offset = offset * 4;
 
-    ((h << (SUBHASH_PAD - bit_offset)) >> SUBHASH_PAD)
+    ((hash << (SUBHASH_PAD - bit_offset)) >> SUBHASH_PAD)
         .try_into()
         .unwrap()
 }
 
-/// BEGIN
-struct KeyEntry {
-    key: u64,
+enum Node {
+    Empty,
+    SubHashTable { entries: Box<[Entry; 16]> },
+    HashKey { key: u64 },
 }
 
-struct SubHashTable {
-    entries: [Option<Box<dyn HashTableEntry>>; 16],
+impl Default for Entry {
+    fn default() -> Self {
+        Entry { node: Node::Empty }
+    }
 }
 
-pub struct HashTrie {
-    root: SubHashTable,
+struct Entry {
+    node: Node,
 }
 
-trait HashTableEntry {
-    fn contains(&self, key: &u64, idx: u8) -> bool;
-    fn sub_hashed(self: Box<Self>) -> Box<SubHashTable>;
-}
+impl Entry {
+    fn add(&mut self, key: u64, depth: u8) {
+        let idx = sub_hash(&key, depth);
+        match &mut self.node {
+            Node::Empty => self.node = Node::HashKey { key: key },
+            Node::SubHashTable { entries } => {
+                &entries[idx].add(key, depth + 1);
+            }
+            Node::HashKey { key: existing_key } if *existing_key != key => {
+                let mut entries: Box<[Entry; 16]> = Box::new(Default::default());
+                &entries[idx].add(key, depth);
 
-impl SubHashTable {
-    fn new() -> Self {
-        SubHashTable {
-            entries: Default::default(),
+                let idx = sub_hash(existing_key, depth);
+                &entries[idx].add(*existing_key, depth);
+                self.node = Node::SubHashTable { entries: entries };
+            }
+            _ => {}
         }
     }
 
-    fn add(&mut self, key: &u64, depth: u8) {
-        let idx = sub_hash4(key, depth);
-        let maybe_entry = &self.entries[idx];
+    fn contains(&self, key: u64, depth: u8) -> bool {
+        match &self.node {
+            Node::Empty => false,
+            Node::HashKey { key: existing_key } => *existing_key == key,
+            Node::SubHashTable { entries } => {
+                entries[sub_hash(&key, depth)].contains(key, depth + 1)
+            }
+        }
+    }
 
-        match maybe_entry {
-            None => self.entries[idx] = Some(Box::new(KeyEntry { key: *key })),
-            Some(entry) => {
-                entry.sub_hashed();
+    fn remove(&mut self, key: u64, depth: u8) {
+        match &mut self.node {
+            Node::Empty => {}
+            Node::HashKey { key: _ } => {
+                self.node = Node::Empty;
+            }
+            Node::SubHashTable { entries } => {
+                let idx = sub_hash(&key, depth);
+                &entries[idx].remove(key, depth + 1);
             }
         }
     }
 }
 
-impl HashTableEntry for KeyEntry {
-    fn contains(&self, key: &u64, _idx: u8) -> bool {
-        *key == self.key
-    }
-
-    fn sub_hashed(self: Box<Self>) -> Box<SubHashTable> {
-        let new_box = Box::new(SubHashTable::new());
-        return new_box;
-    }
-}
-
-impl HashTableEntry for SubHashTable {
-    fn contains(&self, key: &u64, depth: u8) -> bool {
-        let idx = sub_hash4(key, depth);
-        let maybe_entry = &self.entries[idx];
-
-        match maybe_entry {
-            Some(entry) => entry.contains(key, depth + 1),
-            None => false,
-        }
-    }
-
-    fn sub_hashed(self: Box<Self>) -> Box<SubHashTable> {
-        self
-    }
-}
-
-impl HashTrie {
-    fn new() -> Self {
-        HashTrie {
-            root: SubHashTable::new(),
-        }
-    }
-    fn contains(&self, key: &u64) -> bool {
-        self.root.contains(key, 0)
-    }
-}
-
-mod test {
+#[cfg(test)]
+mod tests {
     use super::*;
-
     #[test]
-    fn test_key_entry() {
-        let _key_entry = KeyEntry { key: 42 };
+    fn it_replaces_an_empty_node() {
+        let mut entry = Entry { node: Node::Empty };
+        let hash = calc_hash(&42);
+        entry.add(hash, 0);
+
+        assert_eq!(
+            hash,
+            match entry.node {
+                Node::HashKey { key } => key,
+                _ => unreachable!(),
+            }
+        );
     }
 
     #[test]
-    fn test_sub_hash_table_entry() {
-        let _sub_hash_table_entry = SubHashTable::new();
+    fn it_empty_returns_false() {
+        let entry = Entry { node: Node::Empty };
+        assert!(!entry.contains(100, 0));
     }
 
     #[test]
-    fn test_sub_hash4() {
-        for i in 0..15 {
-            assert_eq!(15, sub_hash4(&u64::max_value(), i));
+    fn it_inserts_into_a_sub_hash_table_node() {
+        let entries: Box<[Entry; 16]> = Box::new(Default::default());
+
+        let mut entry = Entry {
+            node: Node::SubHashTable { entries: entries },
+        };
+        let hash = calc_hash(&42);
+        entry.add(hash, 0);
+        assert!(entry.contains(hash, 0));
+    }
+
+    #[test]
+    fn it_inserts_same_subhash_into_same_sub_hash_table_node() {
+        let mut entry: Entry = Default::default();
+
+        for hash in 0..15 {
+            entry.add(hash, 0);
         }
     }
 }
